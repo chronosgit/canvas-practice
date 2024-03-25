a6e23d058c69a668b533d11b7974f2fa1d937bd3
chronosgit <warfacemeister@mail.ru>
2024-03-25 20:39:14 +0500

    feat & fix: made cropper take optimal size, fixed events related bugs

--- cropper/cropper.js
+++ cropper/cropper.js
@@ -2,12 +2,15 @@
 const loaderWrapper = document.getElementById("loader_wrapper");
 const loaderImg = document.getElementById("loader_img");
 
+let file = null;
+
 const cropperWrapper = document.getElementById("cropper_wrapper");
 
 const cropperContainer = document.getElementById("cropper_container");
 const cropperBox = document.getElementById("cropper_box");
 const cropperImg = document.getElementById("cropper_img");
 const cropperOutline = document.getElementById("cropper_outline");
+const cropperHandlers = document.getElementsByClassName("cropper_handler");
 const cropperHandlerNw = document.getElementById("cropper_handler_nw");
 const cropperHandlerNe = document.getElementById("cropper_handler_ne");
 const cropperHandlerSe = document.getElementById("cropper_handler_se");
@@ -16,18 +19,163 @@ const cropperHandlerSw = document.getElementById("cropper_handler_sw");
 const cropperBtnCrop = document.getElementById("cropper_btn_crop");
 const cropperBtnCancel = document.getElementById("cropper_btn_cancel");
 
+let triggerEl = null;
 let isClicked = false;
-let file = null;
+let opType = null;
+let mouse = {x: undefined, y: undefined};
 
 
 
 /* Cropper logic */
 
-// Translate and resize cropper
+// Functions
+
+function restoreOrigCursors() {
+    cropperOutline.style.cursor = "move";
+    cropperHandlerNe.style.cursor = "ne-resize";
+    cropperHandlerSe.style.cursor = "se-resize";
+    cropperHandlerSw.style.cursor = "sw-resize";
+    cropperHandlerNw.style.cursor = "nw-resize";
+}
+
+function clearStates() {
+    triggerEl = null;
+    isClicked = false;
+    opType = null;
+}
+
+function grab() {
+    console.log("grab");
+}
+
+function neResize() {
+    console.log("ne");
+}
+
+function seResize() {
+    console.log("se");
+}
+
+function swResize() {
+    console.log("sw");
+}
+
+function nwResize() {
+    console.log("nw");
+}
+
+// Outline (box) logic
+
+cropperOutline.addEventListener("mousedown", e => {
+    if(isClicked) return;
+
+    triggerEl = e.currentTarget;
+    isClicked = true;
+    opType = "grab";
+    restoreOrigCursors();
+
+    console.log("Click, so:", opType);
+});
+
+cropperOutline.addEventListener("mousemove", e => {
+    e.preventDefault();
+
+    if(!isClicked) return;
+
+    switch(opType) {
+    case "grab":
+        grab();
+        break;
+    case "ne":
+        neResize();
+        cropperOutline.style.cursor = "ne-resize";
+        break;
+    case "se":
+        seResize();
+        cropperOutline.style.cursor = "se-resize";
+        break;
+    case "sw":
+        swResize();
+        cropperOutline.style.cursor = "sw-resize";
+        break;
+    case "nw":
+        nwResize();
+        cropperOutline.style.cursor = "nw-resize";
+    }
+});
+
+cropperOutline.addEventListener("mouseup", e => {
+    clearStates();
+    restoreOrigCursors();
+
+    console.log("Outline up");
+});
+
+// Handlers logic
+
+for(let i = 0; i < cropperHandlers.length; i++) {
+    const handler = cropperHandlers[i];
+    let type = "";
+
+    if(handler === cropperHandlerNe) type = "ne";
+    else if(handler === cropperHandlerSe) type = "se";
+    else if(handler === cropperHandlerSw) type = "sw";
+    else if(handler === cropperHandlerNw) type = "nw";
+
+    handler.addEventListener("mousedown", e => {
+        if(isClicked) return;
+
+        triggerEl = e.currentTarget;
+        isClicked = true;
+        opType = type;
+
+        console.log("Clicked, so", opType);
+    });
+
+    handler.addEventListener("mousemove", e => {
+        if(!isClicked) return;
+        // Grab is in process, it just hover over
+        if(triggerEl !== handler) {
+            handler.style.cursor = "move";
+        }
+
+        switch(opType) {
+        case "ne":
+            neResize();
+            break;
+        case "se":
+            seResize();
+            break;
+        case "sw":
+            swResize();
+            break;
+        case "nw":
+            nwResize();
+        }
+    });
+
+    handler.addEventListener("mouseup", e => {
+        clearStates();
+        restoreOrigCursors();
+    });
+}
+
+// Fix doc attention (we aint in cropper, so reset)
+document.addEventListener("mousemove", e => {
+    const triggers = [
+        cropperOutline, cropperHandlerNe,
+        cropperHandlerSe, cropperHandlerSw,
+        cropperHandlerNw,
+    ];
+
+    if(!triggers.includes(e.target)) {
+        clearStates();
+    }
+});
 
 
 
-// Cropper init, deinit and action
+/* Cropper init, deinit and action */
 
 function openCropper() {
     cropperWrapper.style.display = "flex";
@@ -37,6 +185,8 @@ function closeCropper() {
     cropperImg.src = "";
 
     cropperWrapper.style.display = "none";
+
+    clearStates();
 }
 
 function crop() {
@@ -71,8 +221,24 @@ loaderWrapper.addEventListener("click", () => {
 
                 cropperImg.src = imgDataUrl;
                 cropperImg.onload = () => {
-                    cropperContainer.style.width = `${cropperImg.clientWidth}px`;
-                    cropperContainer.style.height = `${cropperImg.clientHeight}px`;
+                    const imgW = cropperImg.clientWidth;
+                    const imgH = cropperImg.clientHeight;
+
+                    // To remove weird empty space below some images
+                    cropperContainer.style.width = `${imgW}px`;
+                    cropperContainer.style.height = `${imgH}px`;
+
+                    // To position cropper at full or at center
+                    const isSquare = (imgW - imgH === 0);
+
+                    if(isSquare) {
+                        cropperBox.style.width = "100%";
+                        cropperBox.style.height = "100%";
+                    } else {
+                        console.log(`Zoom: ${imgW, imgH}`);
+                        cropperBox.style.width = `${Math.min(imgW, imgH)}px`;
+                        cropperBox.style.height = `${Math.min(imgW, imgH)}px`;
+                    }
                 };
 
                 openCropper();
@@ -96,8 +262,24 @@ loaderWrapper.addEventListener("drop", (e) => {
 
         cropperImg.src = URL.createObjectURL(droppedFile);
         cropperImg.onload = () => {
-            cropperContainer.style.width = `${cropperImg.clientWidth}px`;
-            cropperContainer.style.height = `${cropperImg.clientHeight}px`;
+            const imgW = cropperImg.clientWidth;
+            const imgH = cropperImg.clientHeight;
+
+            // To remove weird empty space below some images
+            cropperContainer.style.width = `${imgW}px`;
+            cropperContainer.style.height = `${imgH}px`;
+
+            // To position cropper at full or at center
+            const isSquare = (imgW - imgH === 0);
+
+            if(isSquare) {
+                cropperBox.style.width = "100%";
+                cropperBox.style.height = "100%";
+            } else {
+                console.log(`Zoom: ${imgW, imgH}`);
+                cropperBox.style.width = `${Math.min(imgW, imgH)}px`;
+                cropperBox.style.height = `${Math.min(imgW, imgH)}px`;
+            }
         };
 
         openCropper();
