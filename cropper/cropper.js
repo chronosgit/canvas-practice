5da189078fff7433f83c2746533dcbad8fe8440e
chronosgit <warfacemeister@mail.ru>
2024-03-26 23:27:13 +0500

    refactor: refactored comments for 'BetterComments' VSCode extension

--- cropper/cropper.js
+++ cropper/cropper.js
@@ -1,19 +1,18 @@
-/* Setup */
-
+//* Loading related setup
 const loaderWrapper = document.getElementById("loader_wrapper");
 const loaderImg = document.getElementById("loader_img");
-
 let file = null;
 
+//* Cropper related setup
 const cropperArea = document.getElementById("cropper_area");
 const cropperImg = document.getElementById("cropper_img");
 const cropperContainer = document.getElementById("cropper_container");
 
-// Main box, we change it's dims and pos
+//& Main box, we change it's dims and pos
 const cropperBox = document.getElementById("cropper_box");
-// Outline to have events on it, "invisible" trigger area
+//& Outline to have events on it, "invisible" trigger area
 const cropperOutline = document.getElementById("cropper_outline");
-// Circle-like holders for resizing
+//& Circle-like holders for resizing
 const cropperHandlers = document.getElementsByClassName("cropper_handler");
 
 const cropperBtnCrop = document.getElementById("cropper_btn_crop");
@@ -21,8 +20,7 @@ const cropperBtnCancel = document.getElementById("cropper_btn_cancel");
 
 
 
-/* Cropper init, deinit and action */
-
+//* Cropper init, deinit and crop action
 function openCropper() {
     cropperArea.style.display = "flex";
 }
@@ -33,10 +31,9 @@ function closeCropper() {
 }
 
 function crop() {
-    // Temporary image assignment
+    //& Temporary image assignment
     loaderImg.src = cropperImg.src;
 
-    // End cropping
     closeCropper();
 }
 
@@ -45,9 +42,7 @@ cropperBtnCancel.addEventListener("click", closeCropper);
 
 
 
-//* Loading img logic
-
-// Click download (using FileReader and input to load some image to file)
+//* OnClick download (using FileReader and input to load some image to file)
 loaderWrapper.addEventListener("click", () => {
     const tmpInput = document.createElement("input");
     tmpInput.type = "file";
@@ -75,7 +70,7 @@ loaderWrapper.addEventListener("click", () => {
     tmpInput.click();
 });
 
-// DragNDrop download (updating file using drag event's file)
+//* DragNDrop download (updating file using drag event's file)
 loaderWrapper.addEventListener("drop", (e) => {
     e.preventDefault();
 
@@ -99,7 +94,7 @@ function onImgLoad() {
     const imgW = this.clientWidth; // this img
     const imgH = this.clientHeight;
 
-    // To remove weird empty space below some images
+    //& To remove weird empty space below some images
     cropperContainer.style.width = `${imgW}px`;
     cropperContainer.style.height = `${imgH}px`;
 }
